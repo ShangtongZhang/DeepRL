bb0429a5dadf1c4b118c65e48266aae04e86ae25
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2017-04-20 15:20:23 -0600

    Add copyright declaration

--- policy.py
+++ policy.py
@@ -1,3 +1,9 @@
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
 import numpy as np
 
 class GreedyPolicy:
