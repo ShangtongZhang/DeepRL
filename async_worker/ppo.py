4697d1d3e9568e9f4172aa2205eb963b5623f50f
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2017-11-14 19:14:42 -0700

    Remove unnecessary conversion

--- async_worker/ppo.py
+++ async_worker/ppo.py
@@ -95,12 +95,10 @@ class ProximalPolicyOptimization:
 
             values.append(actor_net.to_torch_variable(R))
             A = actor_net.to_torch_variable(torch.zeros((1, 1)))
-            discount = actor_net.to_torch_variable([self.config.discount])
-            gae_tau = actor_net.to_torch_variable([self.config.gae_tau])
             for i in reversed(range(len(rewards))):
                 R = actor_net.to_torch_variable([[rewards[i]]])
-                ret = R + discount * values[i + 1]
-                A = ret - values[i] + discount * gae_tau * A
+                ret = R + self.config.discount * values[i + 1]
+                A = ret - values[i] + self.config.discount * self.config.gae_tau * A
                 advantages.append(A.detach())
                 returns.append(ret.detach())
             advantages = list(reversed(advantages))
