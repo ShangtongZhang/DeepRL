b85ec446aa7997e05ca0a03612a83e3dd171d50a
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-01-01 21:52:24 -0700

    Introduce tensorboardX

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,7 @@
 from .config import *
 from .normalizer import *
 from .misc import *
-
-try:
-    from .tf_logger import Logger
-except:
-    from .vanilla_logger import Logger
-
+from .tf_logger import Logger
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s: %(message)s')
 logger = logging.getLogger('MAIN')
