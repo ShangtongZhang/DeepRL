9b7d21a0f24ca3da7483aafd98359f1474321039
Mike Clark <wassname@users.noreply.github.com>
2018-02-02 15:11:44 +0800

    load and save for SharedReplay
    Not sure if your interested in adding this, it just supports saving and loading SharedReplay. For example if you want to load previous DDPG experience.

--- component/replay.py
+++ component/replay.py
@@ -152,6 +152,22 @@ class SharedReplay:
         with self.buffer_lock:
             return self.sample_()
 
+    def state_dict(self):
+        return dict((key, getattr(self, key)) for key in ['actions', 'states', 'rewards', 'next_states', 'terminals', 'pos'])
+
+    def load_state_dict(self, state):
+        for key in ['actions', 'states', 'rewards', 'next_states', 'terminals', 'pos']:
+            val = state[key]
+            setattr(self, key, val)
+
+    def save(self, file_name):
+        with open(file_name, 'wb') as f:
+            torch.save(self.state_dict(), f)
+
+    def load(self, file_name):
+        state = torch.load(file_name)
+        self.load_state_dict(state)
+
 class HighDimActionReplay:
     def __init__(self, memory_size, batch_size, dtype=np.float32):
         self.memory_size = memory_size
