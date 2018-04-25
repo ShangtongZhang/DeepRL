4f2903eb8f61b1d6be90df295b5fd8d666466d44
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2018-04-25 17:01:05 -0600

    Fix a bug for GPU

--- network/network_utils.py
+++ network/network_utils.py
@@ -12,7 +12,7 @@ import numpy as np
 class BaseNet:
     def set_gpu(self, gpu):
         if gpu >= 0 and torch.cuda.is_available():
-            self.device = torch.device('gpu:%d' % (gpu))
+            self.device = torch.device('cuda:%d' % (gpu))
         else:
             self.device = torch.device('cpu')
         self.to(self.device)
