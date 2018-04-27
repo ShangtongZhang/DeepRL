863974b7fdd9d0c0d3b2c6f88c76c1b53bb98270
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2018-04-27 16:36:03 -0600

    expts

--- network/network_bodies.py
+++ network/network_bodies.py
@@ -27,19 +27,6 @@ class FCBody(nn.Module):
     def __init__(self, state_dim, hidden_units=(64, 64), gate=F.tanh):
         super(FCBody, self).__init__()
         dims = (state_dim, ) + hidden_units
-        self.layers = nn.ModuleList([layer_init(nn.Linear(dim_in, dim_out)) for dim_in, dim_out in zip(dims[:-1], dims[1:])])
-        self.gate = gate
-        self.feature_dim = dims[-1]
-
-    def forward(self, x):
-        for layer in self.layers:
-            x = self.gate(layer(x))
-        return x
-
-class FCBody(nn.Module):
-    def __init__(self, state_dim, hidden_units=(64, 64), gate=F.tanh):
-        super(FCBody, self).__init__()
-        dims = (state_dim, ) + hidden_units
         self.layers = nn.ModuleList([nn.Linear(dim_in, dim_out) for dim_in, dim_out in zip(dims[:-1], dims[1:])])
         self.gate = gate
         self.feature_dim = dims[-1]
