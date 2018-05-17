e9e9172f73426c69d5a944c5c98e3f0c0b67b638
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-05-16 21:08:54 -0600

    N-step QR DQN

---
+++ deep_rl/agent/NStepQRDQN_agent.py
@@ -0,0 +1,87 @@
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
+
+from ..network import *
+from ..component import *
+from ..utils import *
+from .BaseAgent import *
+
+class NStepQRDQNAgent(BaseAgent):
+    def __init__(self, config):
+        BaseAgent.__init__(self, config)
+        self.config = config
+        self.task = config.task_fn()
+        self.network = config.network_fn(self.task.state_dim, self.task.action_dim)
+        self.target_network = config.network_fn(self.task.state_dim, self.task.action_dim)
+        self.optimizer = config.optimizer_fn(self.network.parameters())
+        self.target_network.load_state_dict(self.network.state_dict())
+        self.policy = config.policy_fn()
+
+        self.total_steps = 0
+        self.states = self.task.reset()
+        self.episode_rewards = np.zeros(config.num_workers)
+        self.last_episode_rewards = np.zeros(config.num_workers)
+
+        self.quantile_weight = 1.0 / self.config.num_quantiles
+        self.cumulative_density = self.network.tensor(
+            (2 * np.arange(self.config.num_quantiles) + 1) / (2.0 * self.config.num_quantiles))
+
+    def huber(self, x):
+        cond = (x < 1.0).float().detach()
+        return 0.5 * x.pow(2) * cond + (x.abs() - 0.5) * (1 - cond)
+
+    def iteration(self):
+        config = self.config
+        rollout = []
+        states = self.states
+        for _ in range(config.rollout_length):
+            quantile_values = self.network.predict(self.config.state_normalizer(states))
+            q_values = (quantile_values * self.quantile_weight).sum(-1).cpu().detach().numpy()
+            actions = [self.policy.sample(v) for v in q_values]
+            next_states, rewards, terminals, _ = self.task.step(actions)
+            self.episode_rewards += rewards
+            rewards = config.reward_normalizer(rewards)
+            for i, terminal in enumerate(terminals):
+                if terminals[i]:
+                    self.last_episode_rewards[i] = self.episode_rewards[i]
+                    self.episode_rewards[i] = 0
+
+            rollout.append([quantile_values, actions, rewards, 1 - terminals])
+            states = next_states
+
+            self.policy.update_epsilon()
+            self.total_steps += config.num_workers
+            if self.total_steps / config.num_workers % config.target_network_update_freq == 0:
+                self.target_network.load_state_dict(self.network.state_dict())
+
+        self.states = states
+
+        processed_rollout = [None] * (len(rollout))
+        quantile_values_next = self.target_network.predict(config.state_normalizer(states))
+        a_next = torch.max(quantile_values_next.sum(-1), dim=1)[1]
+        returns = quantile_values_next[self.network.tensor(np.arange(config.num_workers)).long(),
+                  a_next, :].detach()
+        for i in reversed(range(len(rollout))):
+            quantile_values, actions, rewards, terminals = rollout[i]
+            actions = self.network.tensor(actions).long()
+            quantile_values = quantile_values[self.network.tensor(np.arange(config.num_workers)).long(),
+                              actions, :]
+            terminals = self.network.tensor(terminals).unsqueeze(1)
+            rewards = self.network.tensor(rewards).unsqueeze(1)
+            returns = rewards + config.discount * terminals * returns
+            processed_rollout[i] = [quantile_values, returns]
+
+        quantile_values, target_quantile_values = map(lambda x: torch.cat(x, dim=0), zip(*processed_rollout))
+        target_quantile_values = target_quantile_values.t().unsqueeze(-1)
+        diff = target_quantile_values - quantile_values
+        loss = self.huber(diff) * (self.cumulative_density.view(1, -1) - (diff.detach() < 0).float()).abs()
+
+        self.optimizer.zero_grad()
+        loss.mean(1).sum().backward()
+        nn.utils.clip_grad_norm_(self.network.parameters(), config.gradient_clip)
+        self.optimizer.step()
+
+        self.evaluate(config.rollout_length)
---
+++ dist_rl.py
@@ -0,0 +1,83 @@
+from deep_rl import *
+
+def quantile_regression_dqn_cart_pole():
+    config = Config()
+    config.task_fn = lambda: ClassicalControl('CartPole-v0', max_steps=200)
+    config.evaluation_env = config.task_fn()
+    config.optimizer_fn = lambda params: torch.optim.RMSprop(params, 0.001)
+    config.network_fn = lambda state_dim, action_dim: \
+        QuantileNet(action_dim, config.num_quantiles, FCBody(state_dim))
+    config.policy_fn = lambda: GreedyPolicy(epsilon=0.1, final_step=10000, min_epsilon=0.1)
+    config.replay_fn = lambda: Replay(memory_size=10000, batch_size=10)
+    config.discount = 0.99
+    config.target_network_update_freq = 200
+    config.exploration_steps = 100
+    config.logger = get_logger(skip=True)
+    config.num_quantiles = 20
+    run_episodes(QuantileRegressionDQNAgent(config))
+
+def n_step_qr_dqn_cart_pole():
+    config = Config()
+    task_fn = lambda log_dir: ClassicalControl('CartPole-v0', max_steps=200, log_dir=log_dir)
+    # config.evaluation_env = task_fn(None)
+    config.num_workers = 5
+    config.task_fn = lambda: ParallelizedTask(task_fn, config.num_workers)
+    config.optimizer_fn = lambda params: torch.optim.RMSprop(params, 0.001)
+    config.network_fn = lambda state_dim, action_dim: \
+        QuantileNet(action_dim, config.num_quantiles, FCBody(state_dim))
+    config.policy_fn = lambda: GreedyPolicy(epsilon=1.0, final_step=10000, min_epsilon=0.1)
+    config.discount = 0.99
+    config.target_network_update_freq = 200
+    config.rollout_length = 5
+    config.logger = get_logger()
+    config.num_quantiles = 20
+    run_iterations(NStepQRDQNAgent(config))
+
+def n_step_dqn_pixel_atari(name):
+    config = Config()
+    config.history_length = 4
+    task_fn = lambda log_dir: PixelAtari(name, frame_skip=4, history_length=config.history_length, log_dir=log_dir)
+    config.num_workers = 16
+    config.task_fn = lambda: ParallelizedTask(task_fn, config.num_workers,
+                                              log_dir=get_default_log_dir(n_step_dqn_pixel_atari.__name__))
+    config.optimizer_fn = lambda params: torch.optim.RMSprop(params, lr=1e-4, alpha=0.99, eps=1e-5)
+    config.network_fn = lambda state_dim, action_dim: VanillaNet(action_dim, NatureConvBody(), gpu=1)
+    config.policy_fn = lambda: GreedyPolicy(epsilon=1.0, final_step=100000, min_epsilon=0.05)
+    config.state_normalizer = ImageNormalizer()
+    config.reward_normalizer = SignNormalizer()
+    config.discount = 0.99
+    config.target_network_update_freq = 10000
+    config.rollout_length = 5
+    config.gradient_clip = 5
+    config.logger = get_logger()
+    run_iterations(NStepDQNAgent(config))
+
+def n_step_qr_dqn_pixel_atari(name):
+    config = Config()
+    config.history_length = 4
+    task_fn = lambda log_dir: PixelAtari(name, frame_skip=4, history_length=config.history_length, log_dir=log_dir)
+    config.num_workers = 16
+    config.task_fn = lambda: ParallelizedTask(task_fn, config.num_workers,
+                                              log_dir=get_default_log_dir(n_step_qr_dqn_pixel_atari.__name__))
+    config.optimizer_fn = lambda params: torch.optim.RMSprop(params, lr=1e-4, alpha=0.99, eps=1e-5)
+    config.network_fn = lambda state_dim, action_dim: \
+        QuantileNet(action_dim, config.num_quantiles, NatureConvBody(), gpu=2)
+    config.policy_fn = lambda: GreedyPolicy(epsilon=1.0, final_step=100000, min_epsilon=0.05)
+    config.state_normalizer = ImageNormalizer()
+    config.reward_normalizer = SignNormalizer()
+    config.discount = 0.99
+    config.target_network_update_freq = 10000
+    config.rollout_length = 5
+    config.gradient_clip = 5
+    config.logger = get_logger()
+    config.num_quantiles = 200
+    run_iterations(NStepQRDQNAgent(config))
+
+if __name__ == '__main__':
+    # quantile_regression_dqn_cart_pole()
+    # n_step_qr_dqn_cart_pole()
+
+    game = 'BreakoutNoFrameskip-v4'
+
+    n_step_dqn_pixel_atari(game)
+    # n_step_qr_dqn_pixel_atari(game)
