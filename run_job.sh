b3d4241f5e58f1c5c3c0dd186de2f6cefa0611d8
Shangtong Zhang <s84094574@china.huawei.com>
2018-05-25 16:47:48 -0600

    update script

--- run_job.sh
+++ run_job.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-for i in $(seq 0 1); do
+export CUDA_VISIBLE_DEVICES=2
+for i in $(seq 0 0); do
     for j in $(seq 0 2); do
-        nohup python dist_rl.py --ind1 $i --ind2 $j &
+        nohup python dist_rl.py --ind1 $i --ind2 $j >| gpu.txt &
+    done
+done
+
+export CUDA_VISIBLE_DEVICES=3
+for i in $(seq 1 1); do
+    for j in $(seq 0 2); do
+        nohup python dist_rl.py --ind1 $i --ind2 $j >| gpu.txt &
     done
 done
