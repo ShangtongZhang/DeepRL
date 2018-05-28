6b32b60de50a6ba8f06cd45e34b397ca472b0f4b
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2018-05-28 15:51:11 -0600

    expts

--- plot_dist_rl.py
+++ plot_dist_rl.py
@@ -55,9 +55,9 @@ if __name__ == '__main__':
     # plot(pattern='.*log/dist_rl-FreewayNoFrameskip-v4/qr_dqn_pixel_atari.*', figure=1)
     # plt.show()
 
-    game = 'Freeway'
+    # game = 'Freeway'
     # game = 'Seaquest'
-    # game = 'MsPacman'
+    game = 'MsPacman'
     # game = 'Frostbite'
     # game = 'Enduro'
     # game = 'JourneyEscape'
@@ -77,6 +77,10 @@ if __name__ == '__main__':
     # plot(pattern='.*dist_rl_quantile_option_no_skip.*%sNoFrameskip-v4.*mean_and_9_options.*' % (game), figure=0, average=False, color=2, max_timesteps=3e7)
     # plt.show()
 
+    plot(pattern='.*log/dist_rl_deterministic/dist_rl-%sNoFrameskip-v4/option_qr_dqn_pixel_atari.*' % (game), figure=0, color=0)
+    plot(pattern='.*log/dist_rl_deterministic/dist_rl-%sNoFrameskip-v4/qr_dqn_pixel_atari' % (game), figure=0, color=1)
+    plt.show()
+
     # plot(pattern='.*dist_rl_deterministic.*%sNoFrameskip-v4.*original_qr_dqn.*' % (game), figure=0, average=False, color=1, max_timesteps=3e7)
     # plot(pattern='.*dist_rl_deterministic.*%sNoFrameskip-v4.*mean_and_9_options.*' % (game), figure=0, average=False, color=2, max_timesteps=3e7)
     # plt.show()
@@ -92,16 +96,16 @@ if __name__ == '__main__':
     # plot(pattern='.*log/PongNoFrameskip-v4-option-qr-180524-170331.*', figure=0)
     # plt.show()
 
-    kwargs = {
-        'episode_window': 100,
-        'top_k': 5,
-        'max_timesteps': int(3e5),
-        'average': True,
-        'x_interval': 100
-    }
-    plot(pattern='.*log/dist_rl-CliffWalking/qr_dqn_cliff.*', figure=0, color=0, **kwargs)
+    # kwargs = {
+    #     'episode_window': 100,
+    #     'top_k': 5,
+    #     'max_timesteps': int(3e5),
+    #     'average': True,
+    #     'x_interval': 100
+    # }
+    # plot(pattern='.*log/dist_rl-CliffWalking/qr_dqn_cliff.*', figure=0, color=0, **kwargs)
     # plot(pattern='.*log/dist_rl-CliffWalking/option_qr_dqn_cliff/mean_option_qr_dqn.*', figure=0, average=True, max_timesteps=int(2e5), color=1, episode_window=episode_window, x_interval=100)
-    plot(pattern='.*log/dist_rl-CliffWalking/option_qr_dqn_cliff/pure_quantiles_option_qr_dqn.*', figure=0, color=2, **kwargs)
+    # plot(pattern='.*log/dist_rl-CliffWalking/option_qr_dqn_cliff/pure_quantiles_option_qr_dqn.*', figure=0, color=2, **kwargs)
     # plot(pattern='.*log/dist_rl-CliffWalking/option_qr_dqn_cliff/random_option_qr_dqn.*', figure=0, average=True, max_timesteps=int(2e5), color=3, episode_window=episode_window, x_interval=100)
-    plt.show()
+    # plt.show()
 
