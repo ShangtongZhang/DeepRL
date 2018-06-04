576ebbf936f164dabd320e0453e02b0c04f1449d
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2018-06-04 13:02:57 -0600

    expts

--- dist_rl.py
+++ dist_rl.py
@@ -502,18 +502,18 @@ def batch_job():
     cf.add_argument('--ind2', type=int, default=2)
     cf.merge()
 
-    # games = ['FreewayNoFrameskip-v4',
-    #          'BeamRiderNoFrameskip-v4',
-    #          'BattleZoneNoFrameskip-v4',
-    #          'RobotankNoFrameskip-v4',
-    #          'PongNoFrameskip-v4']
-    games = [
-        'BreakoutNoFrameskip-v4',
-        'AssaultNoFrameskip-v4',
-        'JamesbondNoFrameskip-v4',
-        'QbertNoFrameskip-v4',
-        'DemonAttackNoFrameskip-v4'
-    ]
+    games = ['FreewayNoFrameskip-v4',
+             'BeamRiderNoFrameskip-v4',
+             'BattleZoneNoFrameskip-v4',
+             'RobotankNoFrameskip-v4',
+             'PongNoFrameskip-v4']
+    # games = [
+    #     'BreakoutNoFrameskip-v4',
+    #     'AssaultNoFrameskip-v4',
+    #     'JamesbondNoFrameskip-v4',
+    #     'QbertNoFrameskip-v4',
+    #     'DemonAttackNoFrameskip-v4'
+    # ]
     # games = ['FreewayNoFrameskip-v4',
     #          'PongNoFrameskip-v4',
     #          'BattleZoneNoFrameskip-v4',
@@ -565,15 +565,15 @@ if __name__ == '__main__':
     mkdir('log')
     mkdir('data')
     set_one_thread()
-    # batch_job()
+    batch_job()
 
     # bootstrapped_qr_dqn_cliff()
     # bootstrapped_qr_dqn_cliff(option_type='per_step')
     # bootstrapped_qr_dqn_cliff(option_type='per_episode')
     # bootstrapped_qr_dqn_cliff(option_type='decayed_per_step', random_option_prob=LinearSchedule(1.0, 0, int(3e5)))
 
-    parallel = False
-    runs = np.arange(0, 16)
+    # parallel = False
+    # runs = np.arange(0, 16)
     # runs = np.arange(8, 16)
     # multi_runs('CliffWalking', bootstrapped_qr_dqn_cliff, tag='original_qr_dqn', parallel=parallel, runs=runs)
     # multi_runs('CliffWalking', bootstrapped_qr_dqn_cliff, tag='per_step_qr_dqn',
