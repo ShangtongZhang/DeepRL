191eea9bea96908427d55ce499eaa95fefc36857
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2018-06-07 14:53:45 -0600

    update sh

--- run_job.sh
+++ run_job.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 for i in $(seq 0 1); do
     for j in $(seq 0 3); do
-        nohup python ensemble-ddpg.py --ind1 $i --ind2 $j >| nohup.out &
+        nohup python plan-ddpg.py --ind1 $i --ind2 $j >| nohup.out &
     done
 done
