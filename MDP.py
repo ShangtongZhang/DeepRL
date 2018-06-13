430f59d44e4e8c09b73ee2f95b4ee8d7ba56a200
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-06-12 22:07:52 -0600

    complete small MDP example

--- MDP.py
+++ MDP.py
@@ -76,34 +76,38 @@ class QAgent(BaseAgent):
         return total_rewards, self.eval()
 
 class QuantileAgent(BaseAgent):
-    def __init__(self, env_fn, lr=0.1, epsilon=0.1, discount=1.0, num_quantiles=5):
+    def __init__(self, env_fn, lr=0.1, epsilon=0.1, discount=1.0, num_quantiles=5, mean_exploration=False):
         self.env = env_fn()
         self.eval_env = env_fn()
-        # self.q_values = np.zeros((self.env.state_dim, self.env.action_dim, num_quantiles))
-        self.q_values = np.random.randn(*(self.env.state_dim, self.env.action_dim, num_quantiles))
+        self.q_values = np.zeros((self.env.state_dim, self.env.action_dim, num_quantiles))
         self.epsilon = epsilon
         self.discount = discount
         self.lr = lr
         self.num_quantiles = num_quantiles
         self.cumulative_density = (2 * np.arange(num_quantiles) + 1) / (2.0 * num_quantiles)
         self.cumulative_density = torch.FloatTensor(self.cumulative_density)
+        self.mean_exploration = mean_exploration
 
     def act(self, state, eval=False):
         if not eval:
             if np.random.rand() < self.epsilon:
                 return np.random.randint(self.env.action_dim)
             else:
-                q = self.q_values[state].mean(-1)
+                if self.mean_exploration:
+                    q = self.q_values[state, :, :].mean(-1)
+                else:
+                    q = self.q_values[state, :, -1:].mean(-1)
                 best_q = np.max(q)
                 candidates = [i for i in range(self.env.action_dim) if q[i] == best_q]
                 return np.random.choice(candidates)
-        best_q = np.max(self.q_values[state, :, -1])
-        candidates = [i for i in range(self.env.action_dim) if self.q_values[state, i, -1] == best_q]
+        q = self.q_values[state].mean(-1)
+        best_q = np.max(q)
+        candidates = [i for i in range(self.env.action_dim) if q[i] == best_q]
         return np.random.choice(candidates)
 
-    def huber(self, x):
-        cond = (x.abs() < 1.0).float().detach()
-        return 0.5 * x.pow(2) * cond + (x.abs() - 0.5) * (1 - cond)
+    def huber(self, x, k=1.0):
+        cond = (x.abs() < k).float().detach()
+        return 0.5 * x.pow(2) * cond + k * (x.abs() - 0.5 * k) * (1 - cond)
 
     def episode(self):
         state = self.env.reset()
@@ -137,7 +141,6 @@ class QuantileAgent(BaseAgent):
                 break
             state = next_state
 
-        print(self.q_values[:, 1, -1])
         return total_rewards, self.eval()
 
 def run_episodes(agent):
@@ -148,7 +151,8 @@ def run_episodes(agent):
         print('episode %d, return %f, eval return %f' % (ep, online_rewards, eval_rewards))
 
 if __name__ == '__main__':
-    # agent = QAgent(lambda :Chain(10))
+    # agent = QAgent(lambda :Chain(8))
     agent = QuantileAgent(lambda :Chain(8))
+    # agent = QuantileAgent(lambda :Chain(8), mean_exploration=True)
     run_episodes(agent)
 
