5fac6f314ad2a6b8a3d461b828fb4e0172c55882
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2018-06-22 11:28:26 -0600

    expts

--- dist-ddpg.py
+++ dist-ddpg.py
@@ -206,7 +206,7 @@ def batch_job():
         parallel = True
         multi_runs(game, option_ddpg_continuous, tag='b0e0d0', parallel=parallel,
                    beta=0, random_option_prob=LinearSchedule(1.0, 0, int(1e6)), detach=False)
-        multi_runs(game, quantile_ddpg_continuous, tag='q_ddpg')
+        multi_runs(game, quantile_ddpg_continuous, tag='q_ddpg', parallel=parallel)
 
 if __name__ == '__main__':
     mkdir('data')
