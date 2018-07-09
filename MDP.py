84d755961f63e122b71cfafcbce9ffda4cd49c4d
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2018-07-09 15:10:43 -0600

    tabular quantile option

--- MDP.py
+++ MDP.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns;
 sns.set(color_codes=True)
 
+NUM_QUANTILES = 3
+
 class Chain:
     def __init__(self, num_states, up_std=0.1, left_std=1.0):
         self.num_states = num_states
@@ -109,7 +111,7 @@ class QAgent(BaseAgent):
         return total_rewards, None
 
 class QuantileAgent(BaseAgent):
-    def __init__(self, env_fn, lr=0.1, epsilon=0.1, discount=1.0, num_quantiles=5, mean_exploration=False,
+    def __init__(self, env_fn, lr=0.1, epsilon=0.1, discount=1.0, num_quantiles=NUM_QUANTILES, mean_exploration=False,
                  active_quantile=-1):
         self.env = env_fn()
         self.eval_env = env_fn()
@@ -181,6 +183,96 @@ class QuantileAgent(BaseAgent):
         # return total_rewards, self.eval()
         return total_rewards, None
 
+class QuantileOptionAgent(BaseAgent):
+    def __init__(self, env_fn, lr=0.1, epsilon=0.1, discount=1.0, num_quantiles=NUM_QUANTILES, mean_exploration=False):
+        self.env = env_fn()
+        self.eval_env = env_fn()
+        self.q_values = np.zeros((self.env.state_dim, self.env.action_dim, num_quantiles))
+        self.q_options = np.zeros((self.env.state_dim, num_quantiles))
+        self.epsilon = epsilon
+        self.discount = discount
+        self.lr = lr
+        self.num_quantiles = num_quantiles
+        self.cumulative_density = (2 * np.arange(num_quantiles) + 1) / (2.0 * num_quantiles)
+        self.cumulative_density = torch.FloatTensor(self.cumulative_density)
+        self.mean_exploration = mean_exploration
+        self.total_steps = 0
+        self.active_quantile = None
+
+    def act(self, state, eval=False):
+        if not eval:
+            if np.random.rand() < self.epsilon:
+                return np.random.randint(self.env.action_dim)
+            else:
+                if self.mean_exploration:
+                    q = self.q_values[state, :, :].mean(-1)
+                else:
+                    q = self.q_values[state, :, self.active_quantile]
+                best_q = np.max(q)
+                candidates = [i for i in range(self.env.action_dim) if q[i] == best_q]
+                return np.random.choice(candidates)
+        q = self.q_values[state].mean(-1)
+        best_q = np.max(q)
+        candidates = [i for i in range(self.env.action_dim) if q[i] == best_q]
+        return np.random.choice(candidates)
+
+    def huber(self, x, k=1.0):
+        cond = (x.abs() < k).float().detach()
+        return 0.5 * x.pow(2) * cond + k * (x.abs() - 0.5 * k) * (1 - cond)
+
+    def get_option(self, state):
+        if np.random.rand() < self.epsilon:
+            return np.random.randint(self.num_quantiles)
+        q = self.q_options[state]
+        best_q = np.max(q)
+        candidates = [i for i in range(len(q)) if q[i] == best_q]
+        return np.random.choice(candidates)
+
+    def episode(self):
+        state = self.env.reset()
+        self.active_quantile = self.get_option(state)
+        total_rewards = 0.0
+        while True:
+            action = self.act(state)
+            next_state, reward, done, _ = self.env.step(action)
+            self.total_steps += 1
+            total_rewards += reward
+
+            if done:
+                target = np.zeros(self.num_quantiles)
+            else:
+                q_next = self.q_values[next_state, :].mean(-1)
+                a_next = np.argmax(q_next)
+                quantiles_next = self.q_values[next_state, a_next, :]
+                target = self.discount * quantiles_next
+            target += reward
+
+            quantiles = self.q_values[state, action, :]
+
+            quantiles_next = torch.FloatTensor(target).view(-1, 1)
+            quantiles = torch.tensor(quantiles.astype(np.float32), requires_grad=True)
+            diff = quantiles_next - quantiles
+            loss = self.huber(diff) * (self.cumulative_density.view(1, -1) - (diff.detach() < 0).float()).abs()
+            loss = loss.sum(0).mean()
+            loss.backward()
+
+            self.q_values[state, action] -= self.lr * quantiles.grad.numpy().flatten()
+
+            if done:
+                target = 0
+            else:
+                target = self.q_options[next_state, self.active_quantile]
+            target = reward + self.discount * target
+
+            self.q_options[state, self.active_quantile] += self.lr * (target - self.q_options[state, self.active_quantile])
+
+            if done:
+                break
+            state = next_state
+
+        # return total_rewards, self.eval()
+        return total_rewards, None
+
 def check_optimality(q_values):
     if len(q_values.shape) == 3:
         q_values = q_values.mean(-1)
@@ -211,7 +303,8 @@ def upper_quantile_chain():
     agent_fns = [lambda chain_fn: QAgent(chain_fn),
                  lambda chain_fn: QuantileAgent(chain_fn, active_quantile=0),
                  lambda chain_fn: QuantileAgent(chain_fn, active_quantile=-1),
-                 lambda chain_fn: QuantileAgent(chain_fn, mean_exploration=True)]
+                 lambda chain_fn: QuantileAgent(chain_fn, mean_exploration=True),
+                 lambda chain_fn: QuantileOptionAgent(chain_fn)]
     runs = 10
     max_steps = 1e5
     total_steps = np.zeros((len(agent_fns), len(chain_states), runs))
@@ -231,7 +324,7 @@ def plot_upper():
     with open('data/%s.bin' % (upper_quantile_chain.__name__), 'rb') as f:
         steps = pickle.load(f)
     agents = ['Q Learning','Quantile Option (smallest quantile)',
-              'Quantile Option (largest quantile)', 'Quantile Regression']
+              'Quantile Option (largest quantile)', 'Quantile Regression', 'Quantile Option']
     for i, agent in enumerate(agents):
         print(steps[i].mean(-1))
         sns.tsplot(np.transpose(steps[i]), time=np.arange(2, 7), condition=agent,
@@ -245,7 +338,8 @@ def lower_quantile_chain():
     agent_fns = [lambda chain_fn: QAgent(chain_fn),
                  lambda chain_fn: QuantileAgent(chain_fn, active_quantile=0),
                  lambda chain_fn: QuantileAgent(chain_fn, active_quantile=-1),
-                 lambda chain_fn: QuantileAgent(chain_fn, mean_exploration=True)]
+                 lambda chain_fn: QuantileAgent(chain_fn, mean_exploration=True),
+                 lambda chain_fn: QuantileOptionAgent(chain_fn)]
     runs = 10
     max_steps = 1e5
     total_steps = np.zeros((len(agent_fns), len(chain_states), runs))
@@ -265,7 +359,7 @@ def plot_lower():
     with open('data/%s.bin' % (lower_quantile_chain.__name__), 'rb') as f:
         steps = pickle.load(f)
     agents = ['Q Learning','Quantile Option (smallest quantile)',
-              'Quantile Option (largest quantile)', 'Quantile Regression']
+              'Quantile Option (largest quantile)', 'Quantile Regression', 'Quantile Option']
     for i, agent in enumerate(agents):
         print(steps[i].mean(-1))
         sns.tsplot(np.transpose(steps[i]), time=np.arange(2, 9), condition=agent, color=Plotter.COLORS[i],
@@ -284,13 +378,13 @@ if __name__ == '__main__':
     # agent = QuantileAgent(lambda :LowerChain(5, up_std=0.2, left_std=0.1), active_quantile=0)
 
     # original quantile dqn
-    # agent = QuantileAgent(lambda :Chain(8), mean_exploration=True)
+    # agent = QuantileOptionAgent(lambda :Chain(10, up_std=0, left_std=1.0))
     # run_episodes(agent)
 
-    # upper_quantile_chain()
+    upper_quantile_chain()
 
     # lower_quantile_chain()
-    plot_upper()
-    plot_lower()
-    plt.show()
+    # plot_upper()
+    # plot_lower()
+    # plt.show()
 
