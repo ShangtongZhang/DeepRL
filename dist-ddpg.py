6ea4454a792ab0600ddc59dfcc77287d0f2951bb
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2018-07-11 09:38:02 -0600

    expts

--- dist-ddpg.py
+++ dist-ddpg.py
@@ -203,13 +203,13 @@ def batch_job():
         # 'RoboschoolHumanoidFlagrun-v1',
         # 'RoboschoolHumanoidFlagrunHarder-v1',
         # 'RoboschoolInvertedPendulum-v1',
-        'RoboschoolInvertedDoublePendulum-v1',
+        # 'RoboschoolInvertedDoublePendulum-v1',
         'RoboschoolInvertedPendulumSwingup-v1',
     ]
     # game = games[cf.ind1]
 
     for game in games:
-        parallel = True
+        parallel = False
         multi_runs(game, option_ddpg_continuous, tag='b1e0', parallel=parallel,
                    beta=1, random_option_prob=LinearSchedule(1.0, 0, int(1e6)))
         # multi_runs(game, quantile_ddpg_continuous, tag='q_ddpg', parallel=parallel)
