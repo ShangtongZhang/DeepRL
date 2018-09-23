0e18472c23408ab865aae7451b2703fd6506e0c0
alexcoda <alexcoda92@gmail.com>
2018-09-23 14:04:44 -0400

    Reorganized where the updated atari env is located.

--- deep_rl/__init__.py
+++ deep_rl/__init__.py
@@ -3,3 +3,4 @@ from .component import *
 from .model import *
 from .network import *
 from .utils import *
+from .updated_atari_env import *
---
+++ deep_rl/updated_atari_env/__init__.py
@@ -0,0 +1,1 @@
+from .updated_atari_env import to_ram, UpdatedAtariEnv, ACTION_MEANING
