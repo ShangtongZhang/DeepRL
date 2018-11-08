977617733b98b1e19aad7911569de6de1807e961
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2018-11-08 22:23:12 +0000

    Update scripts

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 GPUs=(0 1 2 3 4 5 6 7)
 #for i in $(seq 0 7); do
 #    for j in $(seq 0 4); do
-#        nohup bash docker_python.py ${GPUs[$i]} "job.py --i1 $i --i2 $j" >| job_${i}_${j}.out &
+#        nohup bash docker_python.sh ${GPUs[$i]} "job.py --i1 $i --i2 $j" >| job_${i}_${j}.out &
 #    done
 #done
 
