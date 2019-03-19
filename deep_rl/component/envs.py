5b40a42294aa18fdfadd748833cb3c9e23694a27
Shangtong Zhang <zhangshangtong.cpp@icloud.com>
2019-03-19 09:20:04 +0000

    Update copyright

--- deep_rl/component/envs.py
+++ deep_rl/component/envs.py
@@ -1,5 +1,10 @@
-import os
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
 
+import os
 import gym
 import numpy as np
 import torch
