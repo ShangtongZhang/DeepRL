63d2ac875fcfd654b23b10d74f3255666e352ff2
Shangtong Zhang <zhangshangtong.cpp@gmail.com>
2020-07-05 16:30:07 -0600

    Huber loss

--- template_jobs.py
+++ template_jobs.py
@@ -34,8 +34,8 @@ def batch_atari():
                     [rainbow_pixel, dict(game=game, run=r, n_step=n_step, remark=rainbow_pixel.__name__)]
                 )
             params.append(
-                [categorical_dqn_pixel, dict(game=game, run=r, remark=categorical_dqn_pixel.__name__)]
-            )
+                [categorical_dqn_pixel, dict(game=game, run=r, remark=categorical_dqn_pixel.__name__)]),
+            params.append([dqn_pixel, dict(game=game, run=r, remark=dqn_pixel.__name__)])
 
     algo, param = params[cf.i]
     algo(**param)
